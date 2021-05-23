dd6b95a5432e28d37460f3daf870572eeae88344
Akin Oladeji <lahdayg@yahoo.com>
2021-05-23 11:04:28 +0100

    My First Commit

---
+++ OrangeHRMProjectJune/StepDefinition/AddDeletePayGradesSteps.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace OrangeHRMProjectJune.StepDefinition
+{
+    [Binding]
+    public class AddDeletePayGradesSteps
+    {
+        [Given(@"that I navigate to ""(.*)""")]
+        public void GivenThatINavigateTo(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user enter admin username ""(.*)""")]
+        public void GivenTheUserEnterAdminUsername(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user enter admin password ""(.*)""")]
+        public void GivenTheUserEnterAdminPassword(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user clicks the orange login button")]
+        public void GivenTheUserClicksTheOrangeLoginButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user clicks on admin tab")]
+        public void GivenTheUserClicksOnAdminTab()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user clicks on the job drop down list")]
+        public void GivenTheUserClicksOnTheJobDropDownList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user clicks on Pay Grades")]
+        public void GivenTheUserClicksOnPayGrades()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user clicks on the green Add tab")]
+        public void GivenTheUserClicksOnTheGreenAddTab()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user enters name ""(.*)""")]
+        public void GivenTheUserEntersName(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user clicks on the save button")]
+        public void GivenTheUserClicksOnTheSaveButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user clicks on the Add button")]
+        public void GivenTheUserClicksOnTheAddButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"the user enters currency details ""(.*)""")]
+        public void GivenTheUserEntersCurrencyDetails(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks on save")]
+        public void WhenTheUserClicksOnSave()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks on admin tab")]
+        public void WhenTheUserClicksOnAdminTab()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks on the job drop down list")]
+        public void WhenTheUserClicksOnTheJobDropDownList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks on Pay Grades")]
+        public void WhenTheUserClicksOnPayGrades()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user click in the radio button for fefe")]
+        public void WhenTheUserClickInTheRadioButtonForFefe()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks on the red Delete tab")]
+        public void WhenTheUserClicksOnTheRedDeleteTab()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the user clicks on the green OK button")]
+        public void WhenTheUserClicksOnTheGreenOKButton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"user paygrade should be added successfully")]
+        public void ThenUserPaygradeShouldBeAddedSuccessfully()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the pay grade should be successfully deleted")]
+        public void ThenThePayGradeShouldBeSuccessfullyDeleted()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
